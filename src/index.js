401812f585bb9cd8f3cc1170537dc20892bf6330
gitkazumaa <kazumaalternate@gmail.com>
2023-06-27 09:31:18 -0700

    Add navbar elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+import Icon from "./logos/logo.svg";
+
+function navBar() {
+    const nav = document.createElement("nav");
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    nav.appendChild(myIcon);
+
+    const navList = document.createElement("ul");
+    for (let i = 0; i < 3; i++) {
+        const listItem = document.createElement("li");
+        if (i === 0) {
+            const homeLink = document.createElement("a");
+            homeLink.href = "#";
+            homeLink.innerHTML = "Home";
+            listItem.appendChild(homeLink);
+        } else if (i === 1) {
+            const aboutLink = document.createElement("a");
+            aboutLink.href = "#";
+            aboutLink.innerHTML = "About";
+            listItem.appendChild(aboutLink);
+        } else if (i === 2) {
+            const contactLink = document.createElement("a");
+            contactLink.href = "#";
+            contactLink.innerHTML = "Contact";
+            listItem.appendChild(contactLink);
+        }
+        navList.appendChild(listItem);
+    }
+    nav.appendChild(navList);
+    
+    return nav;
+}
+
+document.body.appendChild(navBar());
